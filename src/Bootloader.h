a70f85f0272aabca37373a978f486964551d4ad9
Mc-StArT <alesha@gilenko.net>
2025-07-03 17:38:04 +0300

    Added new bootloader logic

---
+++ src/Bootloader.h
@@ -0,0 +1,40 @@
+#ifndef BOOTLOADER_H_
+#define BOOTLOADER_H_
+
+#include <stdint.h>
+
+// Set boot memory address depending on platform
+#ifdef STM32F4xx
+    #include "stm32f4xx_hal.h"
+    #define BOOT_ADDR 0x1FFFF000
+#endif
+
+#ifdef STM32F1xx
+    #include "stm32f1xx_hal.h"
+    #define BOOT_ADDR 0x1FFFF000
+#endif
+
+#ifdef STM32H750xx
+    #include "stm32h7xx_hal.h"
+    #define BOOT_ADDR 0x1FF09800
+#endif
+
+// raise warn if boot address is not set
+#ifndef BOOT_ADDR
+    // Looks like unsupported platform
+    #warning "IntroSatLib::EnterBootloader() not supported by selected package"
+    #warning "IntroSatLib::EnterBootloader() currently supports STM32F1xx, STM32F4xx, STM32H750xx."
+    #warning "Calls to IntroSatLib::EnterBootloader() will have no effect"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern void EnterBootloader();
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* IS_BLUETOOTH_H_ */
