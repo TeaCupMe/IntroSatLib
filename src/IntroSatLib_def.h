2005b102e04f14eb15948862d0f1279313b91e87
Mc-StArT <alesha@gilenko.net>
2025-08-30 21:36:14 +0300

    minor cleanup

--- src/IntroSatLib_def.h
+++ src/IntroSatLib_def.h
@@ -3,29 +3,15 @@
 
 #include <stdint.h>
 
-
-
-#ifdef USE_HAL_DRIVER
 enum ISL_StatusTypeDef {
 	ISL_OK      = 0x00U,
 	ISL_ERROR	= 0x01U,
 	ISL_BUSY	= 0x02U,
 	ISL_TIMEOUT	= 0x03U
 };
-#else
-enum ISL_StatusTypeDef: uint8_t {
-	ISL_OK      = 0x00U,
-	ISL_ERROR	= 0x01U,
-	ISL_BUSY	= 0x02U,
-	ISL_TIMEOUT	= 0x03U
-};
-#endif
 
 #define RETURN_STATUS_IF_NOT_OK(func, status) if ((status = func) != ISL_StatusTypeDef::ISL_OK) { return status; }
 #define RETURN_STATUS_IF_NOT_OK_SILENT(func) {ISL_StatusTypeDef __status__ = ISL_StatusTypeDef::ISL_OK; \
 if ((__status__ = func) != ISL_StatusTypeDef::ISL_OK) { return __status__; }}
-
-
-
 
 #endif /* INTROSATLIB_DEF_H_ */
