c400b0fd95f5a8957212532caa8bff7d974951ab
Mc-StArT <alesha@gilenko.net>
2025-09-15 12:01:13 +0300

    comment out beginTransmission()

--- src/Adapter/ARDUINO/ARDUINO_SPI.cpp
+++ src/Adapter/ARDUINO/ARDUINO_SPI.cpp
@@ -4,11 +4,11 @@
 #if defined(ARDUINO)
 
 ISL_StatusTypeDef IntroSatLib::interfaces::SPI::_transfer(const uint8_t* out, uint8_t* in, uint8_t len) {    
-    _hspi->beginTransaction();
+    // _hspi->beginTransaction(); // TODO Надо это делать, но ему нужен аргумент SPISettings. 
     for (int i = 0; i < len; i++) {
         in[i] = _hspi->transfer(out[i]);
     }
-    _hspi->endTransaction();
+    // _hspi->endTransaction();
     return ISL_StatusTypeDef::ISL_OK // always ok?    
 }  
 
