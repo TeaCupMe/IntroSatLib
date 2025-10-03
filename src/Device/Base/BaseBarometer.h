22ac94cfde114ad3d11b52f454554f325428ebb7
Aleksey Gilenko <57070643+TeaCupMe@users.noreply.github.com>
2025-10-03 12:20:48 +0300

    [#40] fix conflicting guard defines (#41)

--- src/Device/Base/BaseBarometer.h
+++ src/Device/Base/BaseBarometer.h
@@ -5,8 +5,8 @@
  *      Author: unflesh
  */
 
-#ifndef BAROMETER_H_
-#define BAROMETER_H_
+#ifndef BASE_BAROMETER_H_
+#define BASE_BAROMETER_H_
 
 #include "Device/Device.h"
 
@@ -21,4 +21,4 @@ public:
 } /* namespace IntroStratLib */
 
 
-#endif /* BAROMETER_H_ */
+#endif /* BASE_BAROMETER_H_ */
