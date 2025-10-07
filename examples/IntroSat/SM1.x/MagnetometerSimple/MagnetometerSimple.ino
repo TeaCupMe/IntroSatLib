f71ea70bdaadd2771217d568a2cf1cd7698f0f4d
Aleksey Gilenko <57070643+TeaCupMe@users.noreply.github.com>
2025-10-07 21:30:55 +0300

    Новая структура папки examples, новые примеры (#39)
    * MotorFlyWheel example added
    
    * Добавлены примеры для работы с IntroSat.
    
    * Исправлены комментарии в примерах.
    
    ---------
    
    Co-authored-by: S-catKas <info@introsat.ru>
    Co-authored-by: unflesh <return.me.my.ship@gmail.com>

--- examples/IntroSat/SM1.x/MagnetometerSimple/MagnetometerSimple.ino
+++ examples/IntroSat/SM1.x/MagnetometerSimple/MagnetometerSimple.ino
@@ -1,19 +1,16 @@
 // Подключение библиотек
 #include <Wire.h>
-#include <AccelerometerV2.h>
+#include <AK8963.h>
 #include <IS_Bluetooth.h>
 
-/**
- * Подключение пространства имён библиотеки,
- * чтобы постоянно не писать IntroSatLib::
- */
+/* Подключение пространства имён библиотеки,
+чтобы постоянно не писать IntroSatLib:: */
 using namespace IntroSatLib;
 
-// Создание объекта акселерометра
-AccelerometerV2 accel(Wire, 0x6B);
+// Создание объекта магнитометра
+AK8963 magn(Wire);
 
-void setup()
-{
+void setup() {
 	// Включение Serial для вывода данных
 	Serial.begin(115200, SERIAL_8E1);
 
@@ -21,39 +18,34 @@ void setup()
 	Wire.begin();
 
 	// Инициализация датчика
-	uint8_t status = accel.Init();
+	uint8_t status = magn.Init();
 
-	/** Проверка успешной инициализации. 
-	 *  	0 - Инициализция успешна
-	 *		1-3 - Ошибка инициализации 
-	 */
-	if (status != 0)
-    {
-        Serial.print("Ошибка инициализации акселерометра! Код ошибки: ");
-        Serial.println(status);
+	/* Проверка успешной инициализации. 
+	  	 	0 - Инициализция успешна
+	 			1-3 - Ошибка инициализации */
+	if (status != 0) {
+		Serial.print("Ошибка инициализации магнитометра! Код ошибки: ");
+		Serial.println(status);
 		Serial.println("Проверьте подключение и адрес датчика!");
 		// Если датчик не инициализирован - уходим в бесконечный цикл
-        while (1)
-            ;
-    }
+		while (1)
+			;
+	}
 }
 
 // Бесконечный цикл - основной код программы
-void loop()
-{
+void loop() {
 	// Выводим данные и названия в бесконечном цикле
-	Serial.print("ax:");
-	Serial.print(accel.X());
-	Serial.print(",ay:");
-	Serial.print(accel.Y());
-	Serial.print(",az:");
-	Serial.println(accel.Z());
+	Serial.print("mx:");
+	Serial.print(magn.X());
+	Serial.print(",my:");
+	Serial.print(magn.Y());
+	Serial.print(",mz:");
+	Serial.println(magn.Z());
 
 	// Проверяем, не пришёл ли запрос на переход в режим перепрошивки
-	if (Serial.available())
-	{
-		if (Serial.read() == 'b')
-		{
+	if (Serial.available()) {
+		if (Serial.read() == 'b') {
 			// Если пришёл символ 'b', переходим в режим перепрошивки
 			enter_bootloader();
 		}
