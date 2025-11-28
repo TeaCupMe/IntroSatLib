3ebf7f9f6da2496a9fe2dd5a8993690b303f1c22
Aleksey Gilenko <57070643+TeaCupMe@users.noreply.github.com>
2025-11-28 17:56:21 +0300

    feat: добавлена поддержку EnterBootloader для IntroSat.Pico (#89)

--- src/ISL_Bootloader.cpp
+++ src/ISL_Bootloader.cpp
@@ -6,8 +6,28 @@ char message_buffer[] = "Bootloader Mode\n\r";
 
 void EnterBootloader(void)
 {
-#ifdef BOOT_ADDR // If platform is supported -> bootloader address is set
-#ifdef STM32F103xB // If F103 (most probably base IntroSat) - print message to UART1
+#ifdef BOOT_ADDR // If bootloader address is set -> platform is supported 
+#ifdef ARDUINO_AVR_ATmega328
+	#ifdef HAVE_HWSERIAL0
+	// If Atmega with Serial available - try to print message
+	Serial.println("Entering bootloader...");
+	Serial.flush();
+	#endif /** HAVE_HWSERIAL0 */
+
+	eeprom_write_byte((uint8_t*)52, 0); // Set flag to stay in bootloader after reset
+    cli();
+    SP = RAMEND;
+    SREG = 0;
+    MCUSR = 0;
+    asm volatile ("eor  __zero_reg__, __zero_reg__");
+    asm volatile ("eor  r1, r1");
+    // jump to after the bootloader checks the reset source
+    asm volatile ("ijmp" ::"z" (BOOT_ADDR / 2));
+    while(1);
+
+#else
+
+#if defined(STM32F103xB) // If F103 (most probably base IntroSat) - print message to UART1
 	
 
     for (char *next_char = message_buffer; *next_char != 0; next_char++)
@@ -50,5 +70,6 @@ void EnterBootloader(void)
 	SysMemBootJump = (void (*)(void)) (*((uint32_t *) (BOOT_ADDR + 4)));
   	__set_MSP(*(uint32_t *)BOOT_ADDR);
   	SysMemBootJump();
+#endif
 #endif /** BOOT_ADDR */ 
 }
