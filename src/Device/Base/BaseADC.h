cebd06f302f13ff7c9cfc75779fb002a7199ae8f
unflesh <return.me.my.ship@gmail.com>
2025-12-03 02:16:39 +0300

    Добавлена работа с ADS7830. BaseADC не готова.

---
+++ src/Device/Base/BaseADC.h
@@ -0,0 +1,19 @@
+#ifndef BASE_ADC_H_
+#define BASE_ADC_H_
+
+#include "Device/Device.h"
+#include "stddef.h"
+
+namespace IntroSatLib {
+
+//template <size_t Channels>
+class BaseADC {
+
+private:
+    //uint8_t resolution;
+public:
+	//virtual float GetValue(size_t channel) = 0;
+};
+}
+
+#endif /* BASE_ADC_H_ */
