84f4b6a211bbb8764c2d7f7fe348a76c7236be6d
Aleksey Gilenko <alesha@gilenko.net>
2025-12-13 04:06:07 +0300

    Добавил поддержку стенда для пикосата pico_stnd 1.0

---
+++ src/PicoStand.cpp
@@ -0,0 +1,33 @@
+#include "PicoStand.h"
+
+IntroSatLib::PicoStand::PicoStand(const interfaces::I2C &i2c, uint8_t address): I2CDevice(new interfaces::I2C(i2c), address)
+{
+    
+}
+
+ISL_StatusTypeDef IntroSatLib::PicoStand::Init()
+{
+    return IsReady();
+}
+
+float IntroSatLib::PicoStand::GetAngle()
+{
+    float angle = 0;
+    ReadRegisterI2C(CMD_GET_ANGLE, (uint8_t*)&angle, 4);
+    return angle;
+}
+
+ISL_StatusTypeDef IntroSatLib::PicoStand::StepCW(int16_t steps)
+{
+    return SetRegisterI2C(CMD_STEP_CW, (uint8_t*)&steps, 2);
+}
+
+ISL_StatusTypeDef IntroSatLib::PicoStand::StepCCW(int16_t steps)
+{
+    return SetRegisterI2C(CMD_STEP_CW, (uint8_t*)&steps, 2);
+}
+
+ISL_StatusTypeDef IntroSatLib::PicoStand::SetAngle(float angle)
+{
+    return SetRegisterI2C(CMD_SET_ANGLE, (uint8_t*)&angle, 4);
+}
