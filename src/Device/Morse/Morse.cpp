f319697009b216da212b412373de8eb15c351fb5
Aleksey Gilenko <alesha@gilenko.net>
2025-12-18 03:21:58 +0300

    Исправил добавление нуль-терминатора в конец принимаемого сообщения в Морзе

--- src/Device/Morse/Morse.cpp
+++ src/Device/Morse/Morse.cpp
@@ -77,7 +77,7 @@ ISL_StatusTypeDef Morse::receive(uint8_t* rx_buffer, uint16_t* length) {
             break;
         }
     }
-    data[*length] = '\0';
+    rx_buffer[*length] = '\0';
 
     return ISL_OK;
 }
