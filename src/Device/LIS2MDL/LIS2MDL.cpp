d83c6ef278319c3c4f940a2ac40a22d3fad3cb2d
TeaCupMe <alesha@gilenko.net>
2026-01-28 03:03:42 +0300

    Исключил LIS2MDL.h из компиляции если периферия I2C недоступна

--- src/Device/LIS2MDL/LIS2MDL.cpp
+++ src/Device/LIS2MDL/LIS2MDL.cpp
@@ -6,7 +6,11 @@
  */
 
 #include "LIS2MDL.h"
+#include "Adapter/I2C.h"
+#include "Device/I2CDevice.h"
 #include "Adapter/System.h"
+
+#ifdef ISL_I2C_ENABLED
 
 namespace IntroSatLib {
 
@@ -77,6 +81,8 @@ float LIS2MDL::MZ()
 LIS2MDL::~LIS2MDL() { }
 
 } /* namespace IntroSatLib */
+
+#endif /* ISL_I2C_ENABLED */
 
 
 
