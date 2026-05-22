84041ed34141fc7c35a84f29397be9e4e2b4d303
GruzinskayaSamsa <matvey28022006@gmail.com>
2026-05-22 12:08:00 +0300

    feat: добавил doxygen комментарии

--- src/Device/ISLIRProtocol/ISLIRProtocol.h
+++ src/Device/ISLIRProtocol/ISLIRProtocol.h
@@ -6,17 +6,24 @@
 namespace IntroSatLib
 {
     namespace IRProtocol
-    {        
+    {
+        /**
+         * \~russian
+         * @brief Временные параметры ИК-протокола.
+         *
+         * Структура задаёт длительности маркеров и пробелов (в условных единицах),
+         * а также допустимую погрешность и максимальные длительности.
+         */
         struct ProtocolTimings {
-            uint16_t markStart = 30;
-            uint16_t spaceStart = 10;
-            uint16_t mark0 = 10;
-            uint16_t mark1 = 20;
-            uint16_t space0 = 10;
-            uint16_t space1 = 10;
-            uint16_t errorScale = 5;
-            uint16_t maxSpaceWidth = 100;
-            uint16_t maxMarkWidth = 30;
+            uint16_t markStart = 30;   ///< \~russian Длительность стартового маркера
+            uint16_t spaceStart = 10;  ///< \~russian Длительность стартового пробела
+            uint16_t mark0 = 10;       ///< \~russian Длительность маркера для бита 0
+            uint16_t mark1 = 20;       ///< \~russian Длительность маркера для бита 1
+            uint16_t space0 = 10;      ///< \~russian Длительность пробела для бита 0
+            uint16_t space1 = 10;      ///< \~russian Длительность пробела для бита 1
+            uint16_t errorScale = 5;   ///< \~russian Допустимое отклонение длительности (scale factor)
+            uint16_t maxSpaceWidth = 100; ///< \~russian Максимальная длительность пробела
+            uint16_t maxMarkWidth = 30;   ///< \~russian Максимальная длительность маркера
         };
     }
 }
--- src/Device/ISLIRTransmitter/ISLIRTransmitter.h
+++ src/Device/ISLIRTransmitter/ISLIRTransmitter.h
@@ -7,10 +7,17 @@
 #include "Adapter/GPIO.h"
 #include "Device/ISLIRProtocol/ISLIRProtocol.h"
 
-
 namespace IntroSatLib
 {
 
+/**
+ * \~russian
+ * @brief Передатчик ИК-сигналов.
+ *
+ * Формирует модулированные посылки согласно заданным временным
+ * параметрам протокола. Использует функции генерации несущей (Tone/NoTone)
+ * и задержек.
+ */
 class ISLIRTransmitter : Device
 {
 
@@ -19,34 +26,101 @@ public:
 protected:
 
 private:
+    interfaces::GPIO txPin;          ///< \~russian Выходной пин передатчика
 
-    interfaces::GPIO txPin;
+    IRProtocol::ProtocolTimings timings = IRProtocol::ProtocolTimings(); ///< \~russian Параметры протокола
+    uint16_t modulationFrequence;     ///< \~russian Частота модуляции (Гц), по умолчанию 38000
 
-    IRProtocol::ProtocolTimings timings = IRProtocol::ProtocolTimings();
-    uint16_t modulationFrequence;
+    void (*Tone)(interfaces::GPIO tonePin, uint16_t frequency) { tone };   ///< \~russian Функция включения несущей
+    void (*NoTone)(interfaces::GPIO tonePin) { noTone };                   ///< \~russian Функция выключения несущей
+    void (*DelaySource)(uint32_t time) {system::Delay};                    ///< \~russian Функция задержки (мкс или мс)
 
-    void (*Tone)(interfaces::GPIO tonePin, uint16_t frequency) { tone };
-    void (*NoTone)(interfaces::GPIO tonePin) { noTone };
-    void (*DelaySource)(uint32_t time) {system::Delay};
-
+    /**
+     * \~russian
+     * @brief Генерирует массив длительностей модуляции для передачи битового потока.
+     *
+     * Преобразует байтовый массив в последовательность интервалов маркер/пробел
+     * согласно протоколу. Каждому биту соответствуют два значения (маркер и пробел).
+     * @param txBuff Указатель на байты для передачи.
+     * @param nBits Количество бит для кодирования.
+     * @param[out] buff Выходной буфер для длительностей.
+     * @param length Максимальная ёмкость выходного буфера.
+     * @return Количество записанных в buff элементов (каждый элемент – длительность в условных единицах).
+     */
     uint16_t GenerateRawTxData(uint8_t* txBuff, uint16_t nBits, uint16_t* buff, uint16_t length);
+
+    /**
+     * \~russian
+     * @brief Отправляет один байт данных.
+     *
+     * Разбирает байт на отдельные биты и для каждого генерирует
+     * необходимую последовательность модуляции.
+     * @param byte Байт для отправки.
+     * @return ISL_OK при успешной отправке, иначе код ошибки.
+     */
     ISL_StatusTypeDef SendByte(uint8_t byte);
 
 public:
-
-    ISLIRTransmitter(interfaces::GPIO _txPin, uint16_t frequence = 38000) : txPin(_txPin), 
+    /**
+     * \~russian
+     * @brief Конструктор.
+     * @param _txPin Выходной пин.
+     * @param frequence Частота модуляции (по умолчанию 38000 Гц).
+     */
+    ISLIRTransmitter(interfaces::GPIO _txPin, uint16_t frequence = 38000) : txPin(_txPin),
                                                                             modulationFrequence(frequence) { }
+    /**
+     * \~russian
+     * @brief Инициализация передатчика.
+     * @return ISL_OK.
+     */
     ISL_StatusTypeDef Init() { return ISL_OK; }
 
+    /**
+     * \~russian
+     * @brief Передать массив байт.
+     *
+     * Генерирует модулированную посылку для всего массива данных.
+     * @param txBuff Указатель на данные.
+     * @param nbytes Количество байт для передачи.
+     * @return ISL_OK при успешной передаче, иначе код ошибки.
+     */
     ISL_StatusTypeDef Transmit(uint8_t* txBuff, uint16_t nbytes);
 
+    /**
+     * \~russian
+     * @brief Назначить функцию включения несущей.
+     * @param _Tone Указатель на функцию с сигнатурой void(interfaces::GPIO, uint16_t).
+     */
     void SetTone(void (*_Tone)(interfaces::GPIO tonePin, uint16_t frequency)) { Tone = _Tone; }
+
+    /**
+     * \~russian
+     * @brief Назначить функцию выключения несущей.
+     * @param _NoTone Указатель на функцию с сигнатурой void(interfaces::GPIO).
+     */
     void SetNoTone(void (*_NoTone)(interfaces::GPIO tonePin)) { NoTone = _NoTone; }
+
+    /**
+     * \~russian
+     * @brief Назначить функцию задержки.
+     * @param _DelaySource Указатель на функцию с сигнатурой void(uint32_t).
+     */
     void SetDelaySource(void (*_DelaySource)(uint32_t time)) { DelaySource = _DelaySource; }
 
+    /**
+     * \~russian
+     * @brief Установить частоту модуляции.
+     * @param _frequence Частота в Гц.
+     */
     void SetModulationFrequence(uint16_t _frequence) { modulationFrequence = _frequence; }
-    uint16_t GetModulationFrequence() { return modulationFrequence; }
 
+    /**
+     * \~russian
+     * @brief Получить текущую частоту модуляции.
+     * @return Частота в Гц.
+     */
+    uint16_t GetModulationFrequence() { return modulationFrequence; }
 };
 
 }
