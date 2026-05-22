91e87bd45cba8f83f65a196f47c95c5be66fd19c
GruzinskayaSamsa <matvey28022006@gmail.com>
2026-05-22 11:17:44 +0300

    fix: исправил методы IRTransceiver

--- src/Device/IRTransceiver/IRTransceiver.cpp
+++ src/Device/IRTransceiver/IRTransceiver.cpp
@@ -1,5 +1,3 @@
-#define ISL_INTERNAL
-
 #include "Device/IRTransceiver/IRTransceiver.h"
 
 #if defined(ARDUINO_AVR_ATmega328)
@@ -8,35 +6,63 @@
 namespace IntroSatLib
 {
 
-    ISL_StatusTypeDef IRTransceiver::Init()
-    {
-        RETURN_STATUS_IF_NOT_OK_SILENT(receiver.Init());
-        RETURN_STATUS_IF_NOT_OK_SILENT(transmitter.Init());
-        return ISL_OK;
-    }
-
-
-    ISL_StatusTypeDef IRTransceiver::TransmitIR(uint8_t* txBuff, uint16_t length)
-    {
-        return transmitter.Transmit(txBuff, length);
-    }
-
-
-    void IRTransceiver::ProcessReceivingIR()
-    {
-        return receiver.ProcessReceiving();
-    }
-
-    uint16_t IRTransceiver::Available()
-    {
-        return receiver.Available();
-    }
-    
-    ISL_StatusTypeDef IRTransceiver::GetData(uint8_t* buff, uint16_t length)
-    {
-        return receiver.GetData(buff, length);
-    }
-
+ISL_StatusTypeDef IRTransceiver::Init()
+{
+    RETURN_STATUS_IF_NOT_OK_SILENT(receiver.Init());
+    RETURN_STATUS_IF_NOT_OK_SILENT(transmitter.Init());
+    return ISL_OK;
 }
 
-#endif
+void IRTransceiver::ProcessReceiving()
+{
+    receiver.ProcessReceiving();
+}
+
+void IRTransceiver::SetTimeSource(uint32_t (*TimeSource)())
+{
+    receiver.SetTimeSource(TimeSource);
+}
+
+uint16_t IRTransceiver::Available()
+{
+    return receiver.Available();
+}
+
+uint16_t IRTransceiver::GetData(uint8_t* buff, uint16_t length)
+{
+    return receiver.GetData(buff, length);
+}
+
+ISL_StatusTypeDef IRTransceiver::Transmit(uint8_t* txBuff, uint16_t nbytes)
+{
+    return transmitter.Transmit(txBuff, nbytes);
+}
+
+void IRTransceiver::SetTone(void (*Tone)(interfaces::GPIO tonePin, uint16_t frequency))
+{
+    transmitter.SetTone(Tone);
+}
+
+void IRTransceiver::SetNoTone(void (*NoTone)(interfaces::GPIO tonePin))
+{
+    transmitter.SetNoTone(NoTone);
+}
+
+void IRTransceiver::SetDelaySource(void (*DelaySource)(uint32_t time))
+{
+    transmitter.SetDelaySource(DelaySource);
+}
+
+void IRTransceiver::SetModulationFrequence(uint16_t freq)
+{
+    transmitter.SetModulationFrequence(freq);
+}
+
+uint16_t IRTransceiver::GetModulationFrequence()
+{
+    return transmitter.GetModulationFrequence();
+}
+
+} // namespace IntroSatLib
+
+#endif // ARDUINO_AVR_ATmega328
--- src/Device/IRTransceiver/IRTransceiver.h
+++ src/Device/IRTransceiver/IRTransceiver.h
@@ -6,39 +6,39 @@
 #include "Device/ISLIRReceiver/ISLIRReceiver.h"
 #include "Device/ISLIRTransmitter/ISLIRTransmitter.h"
 
-
-namespace IntroSatLib {
+namespace IntroSatLib
+{
 
 class IRTransceiver : Device
 {
-public:
-
-protected:
-
 private:
- 
     ISLIRReceiver receiver;
     ISLIRTransmitter transmitter;
 
 public:
+    IRTransceiver(interfaces::GPIO rxPin, interfaces::GPIO txPin, uint16_t modulationFreq = 38000)
+        : receiver(rxPin)
+        , transmitter(txPin, modulationFreq)
+    { };
 
-    IRTransceiver(ISLIRReceiver _receiver, 
-                  ISLIRTransmitter _transmitter): 
-                                                receiver(_receiver), 
-                                                transmitter(_transmitter)  { };
+    ISL_StatusTypeDef Init();
 
-    ISL_StatusTypeDef Init() override;
-
-    ISL_StatusTypeDef TransmitIR(uint8_t* txBuff, uint16_t length);
-    
-    void ProcessReceivingIR();
+    // ----- Receiver interface -----
+    void ProcessReceiving();
+    void SetTimeSource(uint32_t (*TimeSource)());
     uint16_t Available();
-    ISL_StatusTypeDef GetData(uint8_t* buff, uint16_t length);
+    uint16_t GetData(uint8_t* buff, uint16_t length);
 
+    // ----- Transmitter interface -----
+    ISL_StatusTypeDef Transmit(uint8_t* txBuff, uint16_t nbytes);
+    void SetTone(void (*Tone)(interfaces::GPIO tonePin, uint16_t frequency));
+    void SetNoTone(void (*NoTone)(interfaces::GPIO tonePin));
+    void SetDelaySource(void (*DelaySource)(uint32_t time));
+    void SetModulationFrequence(uint16_t freq);
+    uint16_t GetModulationFrequence();
 };
 
+} // namespace IntroSatLib
 
-}
-
-#endif
-#endif
+#endif // ARDUINO_AVR_ATmega328
+#endif // IRTRANSCEIVER_H_
