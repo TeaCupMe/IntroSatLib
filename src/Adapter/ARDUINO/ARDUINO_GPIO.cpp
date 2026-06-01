ab352b81854bd747a17ea370ac6d48d249477e10
Aleksey Gilenko <alesha@gilenko.net>
2026-06-01 16:35:56 +0300

    refactor: GPIO::tone() больше не принимает аргументом pin

--- src/Adapter/ARDUINO/ARDUINO_GPIO.cpp
+++ src/Adapter/ARDUINO/ARDUINO_GPIO.cpp
@@ -12,10 +12,10 @@ void IntroSatLib::interfaces::GPIO::write(uint8_t state) const
 bool IntroSatLib::interfaces::GPIO::isValid() const 
 { return _pin != 0; }
 
-void IntroSatLib::interfaces::GPIO::tone(GPIO_HANDLE_TYPE pin, uint16_t frequency)
-{ ::tone(pin, frequency); }
+void IntroSatLib::interfaces::GPIO::tone(uint16_t frequency)
+{ ::tone(_pin, frequency); }
 
-void IntroSatLib::interfaces::GPIO::noTone(GPIO_HANDLE_TYPE pin)
-{ ::noTone(pin);}
+void IntroSatLib::interfaces::GPIO::noTone()
+{ ::noTone(_pin);}
 
 #endif /* defined(HAL_GPIO_MODULE_ENABLED) && !defined(ARDUINO) */
