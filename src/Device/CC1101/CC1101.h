ec4869defd702f14da6fdd47f09db5ad174b19ff
Aleksey Gilenko <alesha@gilenko.net>
2026-06-01 20:57:30 +0300

    feat: добавлена возможность передавать const-указатель в CC1101::transmit()

--- src/Device/CC1101/CC1101.h
+++ src/Device/CC1101/CC1101.h
@@ -154,6 +154,11 @@ public:
 	void setSyncWord(uint16_t sync);
 
 	Status transmit(uint8_t *data, size_t length, uint8_t addr = 0);
+	Status transmit(const uint8_t *data, size_t length, uint8_t addr = 0)
+	{
+		return transmit(const_cast<uint8_t*>(data), length, addr);
+	}
+
 	Status receive(uint8_t *data, size_t length, size_t *read = nullptr,
 			uint8_t addr = 0);
 	int8_t getRSSI();
