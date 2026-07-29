2188627392586985f4f2b1332fb86abc829d20c6
Aleksey Gilenko <57070643+TeaCupMe@users.noreply.github.com>
2026-07-29 13:31:44 +0300

    feat: добавил GetMicros() и DelayMicros() (#131)
    * feat: добавил GetMicros() и DelayMicros()
    
    * fix: убрал неподдерживаемый include
    
    * fix: исправление one-off ошибки в system::GetMicros под STM32

--- src/Adapter/System.h
+++ src/Adapter/System.h
@@ -50,7 +50,10 @@
 namespace IntroSatLib {
 	namespace system {
 		uint32_t GetTick();
+		const auto GetMillis = GetTick; // Alias for GetTick, as millis() is more common in Arduino and HAL_GetTick() is more common in STM32 HAL
+		uint32_t GetMicros();
 		void Delay(uint32_t milliseconds);
+		void DelayMicroseconds(uint32_t microseconds);
 	};
 };
 
