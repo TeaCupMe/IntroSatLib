12ccdd7667ba7d2eebc8aaed800d27bc093c371e
Aleksey Gilenko <alesha@gilenko.net>
2026-08-20 15:30:54 +0300

    feat: исправление типов для pl_psm

--- src/Platform/PSM/Types.h
+++ src/Platform/PSM/Types.h
@@ -4,17 +4,17 @@
 namespace platform::psm::types {
 
 // Heater
-enum HeaterMode: uint8_t {
-	HEATER_MODE_AUTO = 0x00,
-	HEATER_MODE_MANUAL = 0x01,
-	HEATER_MODE_OFF = 0x02
+enum class HeaterMode: uint8_t {
+	Auto,
+	Manual,
+	Off,
 };
 
-enum HeaterState: uint8_t {
-	HEATER_STATE_ON = 0x1,
-	HEATER_STATE_ON_AUTO = 0x3,
-	HEATER_STATE_OFF = 0x0,
-	HEATER_STATE_OFF_AUTO = 0x2
+enum class HeaterState: uint8_t {
+	On,
+	OnAuto,
+	Off,
+	OffAuto
 };
 
 struct HeaterInfo {
@@ -26,50 +26,55 @@ struct HeaterInfo {
 };
 
 
-enum PowerMode: uint8_t {
-	POWER_MODE_MANUAL = 0x0,
-	POWER_MODE_NORMAL = 0x3, //
-	POWER_MODE_ALL_ON = 0x6,
-	POWER_MODE_ALL_OFF = 0x9
+enum class PowerMode: uint8_t {
+	Manual,
+	Normal,
+	AllOn,
+	AllOff
 };
 
-enum PowerChannelState: uint8_t {
-	DISABLED = 0x0,
-	ENABLED = 0x1,
-	DISABLED_OVERCURRENT = 0x2,
-	DISABLED_UNDERVOLTAGE = 0x3
+enum class ChannelState: uint8_t {
+	Off,
+	On,
+	OffOvercurrent,
+	OffUndervoltage
 };
 
 struct __attribute__((__packed__)) ChannelInfo {
-	PowerChannelState state;
+	ChannelState state;
 	float voltage;
 	float current;
 	float peakCurrent;
 };
 
+
 struct __attribute__((__packed__)) ExtendedChannelInfo: ChannelInfo {
 	uint16_t rawVoltage; // 12-bit from ADC
 	uint16_t rawCurrent; // 12-bit from ADC
-
+	
 	uint32_t lastDisabled = 0;
 	uint32_t lastOvercurrent = 0;
 	uint32_t lastUndervoltage = 0;
 	uint32_t lastEnabled = 0;
+	//	float powerUsage; // continuous integration??
+};
 
+struct __attribute__((__packed__)) ChannelConfig {
+	ChannelState state = ChannelState::Off;
 	bool enableOvercurrentProtection = false;
 	bool enableUndervoltageProtection = false;
 	float overCurrentThreshold = INT64_MAX;
 	float underVoltageThreshold = 0;
-	//	float powerUsage; // continuous integration??
 };
 
-enum PowerChannel: uint8_t {
-	POWER_CHANNEL_12V 		= 0x1,
-	POWER_CHANNEL_12V_IS 	= 0x2,
-	POWER_CHANNEL_3V3 		= 0x3,
-	POWER_CHANNEL_3V3_IS 	= 0x4,
-	POWER_CHANNEL_5V 		= 0x5,
-	POWER_CHANNEL_5V_IS 	= 0x6
+enum class PowerChannel: uint8_t {
+	Battery,
+	PayloadBattery,
+	Main3v3,
+	Payload3v3,
+	Main5v,
+	Payload5v,
+	Qty
 };
 
 } /* namespace platform::psm::types */
